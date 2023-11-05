99d0108091f25a83e4e42ea250bd45744a868151
Ehsan <esihazar@gmail.com>
2023-11-05 13:06:29 -0500

    League page optmized for mobile format

--- soccer-league/src/LeaguePage/League.jsx
+++ soccer-league/src/LeaguePage/League.jsx
@@ -4,59 +4,101 @@ import "../LeaguePage/style.css";
 import "./style.css";
 function League({ leaguTable, homeGame, awayGame }) {
   const [leagueData, setLeagueData] = useState([]);
-
+  const [showAllColumn, setShowAllColumn] = useState(true);
+  const [screenSize, setScreenSize] = useState();
   //If you dont do this after refreshing the page it is not going to display your table dat
   useEffect(() => {
     setLeagueData(leaguTable);
+
+    // Create a function to handle the media query change
+    const handleScreenSizeChange = (e) => {
+      if (e.matches) {
+        console.log("false");
+        setShowAllColumn(false);
+      } else {
+        console.log("true");
+        setShowAllColumn(true);
+      }
+    };
+
+    // Add the listener and call the handler initially
+    const screenSize = window.matchMedia("(max-width: 768px)");
+    screenSize.addListener(handleScreenSizeChange);
+    handleScreenSizeChange(screenSize); // Call the handler initially
+
+    // Cleanup the listener when the component unmounts
+    return () => {
+      screenSize.removeListener(handleScreenSizeChange);
+    };
   }, [leaguTable]);
 
   const data = React.useMemo(() => leagueData, [leagueData]);
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: "Rank",
-        accessor: "rank",
-      },
-      {
-        Header: "Team",
-        accessor: "team_name",
-      },
-      {
-        Header: "Points",
-        accessor: "points",
-      },
-      {
-        Header: "Games Played",
-        accessor: "games_played",
-      },
-      {
-        Header: "Wins",
-        accessor: "won",
-      },
-      {
-        Header: "Draw",
-        accessor: "draw",
-      },
-      {
-        Header: "Lost",
-        accessor: "lost",
-      },
-      {
-        Header: "GD",
-        accessor: "goals_diff",
-      },
-      {
-        Header: "GF",
-        accessor: "goals_for",
-      },
-      {
-        Header: "GA",
-        accessor: "goals_against",
-      },
-    ],
-    []
-  );
+  const columns = React.useMemo(() => {
+    if (showAllColumn) {
+      return [
+        {
+          Header: "Rank",
+          accessor: "rank",
+        },
+        {
+          Header: "Team",
+          accessor: "team_name",
+        },
+        {
+          Header: "Points",
+          accessor: "points",
+        },
+        {
+          Header: "Games Played",
+          accessor: "games_played",
+        },
+        {
+          Header: "Wins",
+          accessor: "won",
+        },
+        {
+          Header: "Draw",
+          accessor: "draw",
+        },
+        {
+          Header: "Lost",
+          accessor: "lost",
+        },
+        {
+          Header: "GD",
+          accessor: "goals_diff",
+        },
+        {
+          Header: "GF",
+          accessor: "goals_for",
+        },
+        {
+          Header: "GA",
+          accessor: "goals_against",
+        },
+      ];
+    } else {
+      return [
+        {
+          Header: "Rank",
+          accessor: "rank",
+        },
+        {
+          Header: "Team",
+          accessor: "team_name",
+        },
+        {
+          Header: "Points",
+          accessor: "points",
+        },
+        {
+          Header: "Games Played",
+          accessor: "games_played",
+        },
+      ];
+    }
+  }, [showAllColumn]);
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns, data });
@@ -69,7 +111,10 @@ function League({ leaguTable, homeGame, awayGame }) {
             {headerGroups.map((headerGroup) => (
               <tr {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
+                  <th
+                    {...column.getHeaderProps()}
+                    className={column.render("Header")}
+                  >
                     {column.render("Header")}
                   </th>
                 ))}
